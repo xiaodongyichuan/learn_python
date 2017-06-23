7054edb9bfc1855d1186d68386c13e73a6995d09
kungfuqian <958819543@qq.com>
2017-06-23 14:46:29 +0800

    Create 13  handwritten digital.py

---
+++ 13  handwritten digital.py
@@ -0,0 +1,4 @@
+# Goal
+
+# The goal in this competition is to take an image of a handwritten single digit, and determine what that digit is.
+# For every ImageId in the test set, you should predict the correct label.
